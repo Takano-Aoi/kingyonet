7802009bd1f4d9e9edb695f1b33936a8ceed48c1
Aoi Takano <sj3bmeeiiuvjb20jy3we@docomo.ne.jp>
2023-11-12 19:51:47 +0900

    masterKey0890f09f69545063adb8a1be3eab7d90

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
-
-
-Rails.application.routes.draw do
   resources :fishes
   resources :kinds
   resources :pets
@@ -12,5 +7,5 @@ Rails.application.routes.draw do
   resources :grows
   resources :diseases
   resources :homes
-  'root#index'
+  root 'homes#index'
 end
